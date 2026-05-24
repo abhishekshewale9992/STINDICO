9bb1f0764011b0194079566f184b38eda418cd7a
Abhishek Shewale <abhishekshewale9992@gmail.com>
2026-05-24 23:26:26 +0530

    fix final github pages url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://abhishekshewale9992.github.io',
+  site: 'https://abhishekshewale9992.github.io/STINDICO/',
   base: '/STINDICO',
   trailingSlash: 'always',
   image: {
